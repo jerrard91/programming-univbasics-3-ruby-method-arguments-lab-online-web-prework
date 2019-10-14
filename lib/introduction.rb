352a96a33beb5ac38ecf94ecbcd643475f1e2a54
jerrard suell <seulljay@gmail.com>
2019-10-14 17:50:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,10 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+end
+
 
 
 
